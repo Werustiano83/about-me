6858407b9535fbbe99e4d2714bbe8e124ccd18d5
Raul Villareal <rvillarrcal@cnm.edu>
2016-07-13 08:38:08 -0600

    added color to my name

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
-	<html lang="en">
+<html lang="en">
 	<head>
 		<meta charset="UTF-8"/>
 		<link href="css/style.css" rel="stylesheet" type="text/css"/>
 		<title> About ME!</title>
 	</head>
 	<body>
-	<main>
-		<h1> Good day Everybody!!</h1>
-		<p> My name is Raúl Villarreal; friend of not so many, cousin of very many, sibling of three, son of two, father of one princess and two dudes and most importantly, husband of only one. As many of my future colleagues and current co-workers I have realized how technology can be a great tool to improve lives. It can improve personal lives as well as improve communities in a very effective way. That is the reason why I want to develop as many ideas as possible. I am aware that great ideas can come from anybody and anywhere; and yours could be the next great idea I can help materialize if you let me. Tell me which is the great idea you have in mind. We'll talk about it and will start improving yours and others' lives really soon. </p>
-			<p>
-			<h2> These are the websites that inspire me!!</h2>
-				<img src="images/khan.png" alt="Khan Academy" />
-				<a href="https://khanacademy.org" target="blank"> Khan Academy </a>
-				<p> I love Khan Academy because is decentralizing education, the idea of people learning whatever they want, whenever they want, free of charge is just mindblowing!!! I encourage you to explore it and think about what technology can do for the world.</p>
-				<img src="images/timebanks.jpg" alt="Time Banks" />
-				<a href="https://timebanks.org" target="blank"> TimeBanks </a>
-				<p> Time Banks is a great idea, regular people get to exchange their skills and servies without using cash. In my case, in exchange for cleaning carpets I can get babysitting, music lessons, massage therapy and much, much more.</p>
-				<img src="images/gofundme.png" alt="Go Fund Me" />
-				<a href="https://gofundme.com" target="_blank"> Go Fund Me </a>
-				<p>Crowdfunding is to connect those in financial need with those that have the resources to support them. People have obtained help to pay for medical bills, school tuition and personal projects among much other things.</p>
-			<h2> These are my pending projects:</h2>
-			<h2> This is what makes me happy:</h2>
+			<main>
+				<h1>Raúl Villarreal</h1>
+					<p> Hello everybody!!! I'm a friend of not so many, cousin of very many, sibling of three, son of two, father of one princess and two dudes and most importantly, husband of only one. As many of my future colleagues and current co-workers I have realized how technology can be a great tool to improve lives. It can improve personal lives as well as improve communities in a very effective way. That is the reason why I want to develop as many ideas as possible. I am aware that great ideas can come from anybody and anywhere; and yours could be the next great idea I can help materialize if you let me. Tell me which is the great idea you have in mind. We'll talk about it and will start improving yours and others' lives really soon. </p>
+					<p>
+				<h2> These are the websites that inspire me!!</h2>
+					<img src="images/khan.png" alt="Khan Academy" />
+					<a href="https://khanacademy.org" target="blank"> Khan Academy </a>
+					<p> I love Khan Academy because is decentralizing education, the idea of people learning whatever they want, whenever they want, free of charge is just mindblowing!!! I encourage you to explore it and think about what technology can do for the world.</p>
+					<img src="images/timebanks.jpg" alt="Time Banks" />
+					<a href="https://timebanks.org" target="blank"> TimeBanks </a>
+					<p> Time Banks is a great idea, regular people get to exchange their skills and servies without using cash. In my case, in exchange for cleaning carpets I can get babysitting, music lessons, massage therapy and much, much more.</p>
+					<img src="images/gofundme.png" alt="Go Fund Me" />
+					<a href="https://gofundme.com" target="_blank"> Go Fund Me </a>
+					<p>Crowdfunding is to connect those in financial need with those that want to help, even with micro donations. People have obtained help to pay for medical bills, school tuition and personal projects among much other things.</p>
+				<h2> These is my portfolio of projects:</h2>
+				<h2> This is what makes me happy:</h2>
 		</main>
 		</body>
+</html>
