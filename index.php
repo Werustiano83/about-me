8729741638baa6330305f663ae5a675ae8fc1df0
Raul Villareal <rvillarrcal@cnm.edu>
2016-07-15 08:31:17 -0600

    fixed style (i guess)

--- index.php
+++ index.php
@@ -5,11 +5,14 @@
 		<link href="css/style.css" rel="stylesheet" type="text/css"/>
 		<title> About ME!</title>
 	</head>
+
 	<body>
 			<main>
 				<h1>Raúl Villarreal</h1>
 					<p> Hello everybody!!! I'm a friend of not so many, cousin of very many, sibling of three, son of two, father of one princess and two dudes and most importantly, husband of only one. As many of my future colleagues and current co-workers I have realized how technology can be a great tool to improve lives. It can improve personal lives as well as improve communities in a very effective way. That is the reason why I want to develop as many ideas as possible. I am aware that great ideas can come from anybody and anywhere; and yours could be the next great idea I can help materialize if you let me. Tell me which is the great idea you have in mind. We'll talk about it and will start improving yours and others' lives really soon. </p>
-					<h2> These are the websites that inspire me!!</h2>
+
+				<h2> These are the websites that inspire me!!</h2>
+
 						<div class="img">
 							<a href="https://khanacademy.org" target="blank">
 								<img src="images/khan.png" alt="Khan Academy" />
@@ -18,24 +21,28 @@
 						<div class="desc">
 							<p> I love Khan Academy because is decentralizing education, the idea of people learning whatever they want, whenever they want, free of charge is just mindblowing!!! I encourage you to explore it and think about what technology can do for the world.</p>
 						</div>
-					<div class="img">
-						<a href="https://timebanks.org" target="blank">
-							<img src="images/timebanks.jpg" alt="Time Banks" />
-						</a>
-					</div>
+
+						<div class="img">
+							<a href="https://timebanks.org" target="blank">
+								<img src="images/timebanks.jpg" alt="Time Banks" />
+							</a>
+						</div>
 						<div class="desc">
 							<p> Time Banks is a great idea, regular people get to exchange their skills and servies without using cash. In my case, in exchange for cleaning carpets I can get babysitting, music lessons, massage therapy and much, much more.</p>
 						</div>
-					<div class="img">
-						<a href="https://gofundme.com" target="_blank">
-							<img src="images/gofundme.png" alt="Go Fund Me"  />
-						</a>
-					</div>
-					<div class="desc">
-					<p>Crowdfunding is to connect those in financial need with those that want to help, even with micro donations. People have obtained help to pay for medical bills, school tuition and personal projects among much other things.</p>
-					</div>
+
+						<div class="img">
+							<a href="https://gofundme.com" target="_blank">
+								<img src="images/gofundme.png" alt="Go Fund Me"  />
+							</a>
+						</div>
+						<div class="desc">
+							<p>Crowdfunding is to connect those in financial need with those that want to help, even with micro donations. People have obtained help to pay for medical bills, school tuition and personal projects among much other things.</p>
+						</div>
+
 				<h2> This is my portfolio of projects:</h2>
+
 				<h2> This is what makes me happy:</h2>
 		</main>
-		</body>
+	</body>
 </html>
