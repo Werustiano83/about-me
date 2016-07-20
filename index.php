9664a89c960178b197a56d3c7db77131be7e5fd3
Raul Villareal <rvillarrcal@cnm.edu>
2016-07-20 13:03:22 -0600

    cool background and background attachment

--- index.php
+++ index.php
@@ -10,10 +10,8 @@
 			<main>
 				<h1>Raúl Villarreal</h1>
 					<p> Hello everybody!!! I'm a friend of not so many, cousin of very many, sibling of three, son of two, father of one princess and two dudes and most importantly, husband of only one. As many of my future colleagues and current co-workers I have realized how technology can be a great tool to improve lives. It can improve personal lives as well as improve communities in a very effective way. That is the reason why I want to develop as many ideas as possible. I am aware that great ideas can come from anybody and anywhere; and yours could be the next great idea I can help materialize if you let me. Tell me which is the great idea you have in mind. We'll talk about it and will start improving yours and others' lives really soon. </p>
-
-				<!-- Websites that I love-->
+								<!-- Websites that I love-->
 				<h2> These are the websites that inspire me!!</h2>
-
 				<!-- Added image, description and link for three websites-->
 						<div class="img">
 							<a href="https://khanacademy.org" target="blank">
@@ -45,6 +43,6 @@
 				<h2> This is my portfolio of projects:</h2>
 
 				<h2> This is what makes me happy:</h2>
-		</main>
+			</main>
 	</body>
 </html>
